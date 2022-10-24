9d62d33d71d7ec02946952dc95ea5d66a679ba07
Ganapathy Duraiswamy <ganadurai@google.com>
2022-10-24 16:32:06 -0500

    auto install

--- scripts/single-node-install.sh
+++ scripts/single-node-install.sh
@@ -35,13 +35,21 @@ function fetchHybridInstall() {
 }
 
 function dockerInstall() {
-  sudo apt update;
-  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-  apt-cache policy docker-ce
-  sudo apt install docker-ce -y
-  sudo systemctl status docker
+  sudo apt-get update
+
+  sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+  sudo add-apt-repository "deb [arch=amd64] 
+  https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+  sleep 20
+
+  sudo apt-get update
+
+  sudo apt install --yes docker-ce
+
   sudo usermod -aG docker "${USER}"
   su - "${USER}"
 }
