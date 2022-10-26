f6604ad2dd1e3f60c40b6f1efaa3448490a2e303
Ganapathy Duraiswamy <ganadurai@google.com>
2022-10-25 21:46:49 -0500

    auto install

---
+++ scripts/installDocker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+sudo apt-get update
+sudo apt install --yes apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+echo "Waiting for 10s..."
+sleep 10
+sudo apt-get update
+sudo apt install --yes docker-ce
+
+sudo usermod -aG docker "$USER"
+logout
