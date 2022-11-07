1c706fc2fd47446b127057df182aedbe30ef6fe3
Ganapathy Duraiswamy <ganadurai@google.com>
2022-11-06 21:40:00 -0600

    fixes

--- scripts/install-functions.sh
+++ scripts/install-functions.sh
@@ -192,7 +192,7 @@ function hybridInstall() {
 function certManagerInstall() {
   cd "$HYBRID_INSTALL_DIR"
   echo "checking cert manager exixts"
-  RESULT=kubectl get namespace | { grep cert-manager || true; } | wc -l
+  RESULT=$(kubectl get namespace | { grep cert-manager || true; } | wc -l); echo "$RESULT"
   echo "checked cert manager exists"
   if [[ $RESULT -eq 0 ]]; then
     kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.7.2/cert-manager.yaml
