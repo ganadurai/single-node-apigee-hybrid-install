ad05c4710383ba64690d770ee2d81c81d54d907c
Ganapathy Duraiswamy <ganadurai@google.com>
2022-11-09 04:45:47 -0600

    setups

---
+++ terraform-modules/apigee-install/main.tf
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ locals {
+  env_groups = var.apigee_envgroups
+}
+
+module "apigee" {
+  source              = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/apigee-organization?ref=v16.0.0"
+  project_id          = module.project.project_id
+  analytics_region    = var.ax_region
+  runtime_type        = "HYBRID"
+  apigee_environments = var.apigee_environments
+  apigee_envgroups    = var.apigee_envgroups
+  count               = var.apigee_org_create ? 1 : 0
+}
