e6809a4079319e71c438529363a3aa1624256076
Ganapathy Duraiswamy <ganasdurai@gmail.com>
2024-05-27 17:05:44 -0500

    clean up of copyright text

--- envoy/envoy-entrypoint.sh
+++ envoy/envoy-entrypoint.sh
@@ -4,7 +4,5 @@ set -e
 echo "Generating envoy.yaml config file..."
 envsubst < /tmpl/envoy.yaml.tmpl > /etc/envoy.yaml
 
-#cat /tmpl/envoy.yaml.tmpl | envsubst \$APIGEE_NAMESPACE,\$SERVICE_NAME > /etc/envoy.yaml
-
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
