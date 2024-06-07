db25b5d498728d8e43a21815d27e61318b3810a2
Ganapathy Duraiswamy <ganasdurai@gmail.com>
2024-06-07 17:23:48 -0500

    add eks setup script - fixes

--- scripts/setup-eks.sh
+++ scripts/setup-eks.sh
@@ -37,7 +37,7 @@ function installEksctl() {
 function checkRoleExists() {
     OUTPUT=$(aws iam get-role --role-name $1 2> /dev/null)
     RESULT=$?
-
+    echo "done"
     #Delete role and detach role policy, if it already exists 
     if [ $RESULT -eq 0 ]; then
         echo "success"
