d187ccfe2fbb863b26f20cc6ef80e257419621bf
Ganapathy Duraiswamy <ganasdurai@gmail.com>
2024-06-07 17:22:15 -0500

    add eks setup script - fixes

--- scripts/setup-eks.sh
+++ scripts/setup-eks.sh
@@ -48,7 +48,7 @@ function checkRoleExists() {
 
 function prepEksClusterRole() {
   
-    OUTPUT=$(checkRoleExists("myAmazonEKSClusterRole"))
+    OUTPUT=$(checkRoleExists "myAmazonEKSClusterRole")
     RESULT=$?
 
     #Delete role and detach role policy, if it already exists 
